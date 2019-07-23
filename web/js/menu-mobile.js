51d18fad761618aacd0a03788277b6abe45b340c
carcheky <carcheky@gmail.com>
2019-07-23 23:57:45 +0200

    works!

--- web/js/menu-mobile.js
+++ web/js/menu-mobile.js
@@ -1,24 +1,32 @@
-$(function() {
-  $('#main-nav ul.main-menu').smartmenus();
-
-  $('.closed button.toggle').click(function() {
-    $('#main-nav.closed ul.main-menu').toggle();
-    $('#main-nav.closed button.toggle').attr('aria-expanded', 'true');
-    $('#main-nav.closed').addClass('opened');
-    $('#main-nav.opened.closed').removeClass('closed');
+// SmartMenus init
+$(function () {
+  $('#main-menu').smartmenus({
+    mainMenuSubOffsetX: -1,
+    subMenusSubOffsetX: 10,
+    subMenusSubOffsetY: 0
   });
+});
 
-  $('.opened button.toggle').click(function() {
-    $('#main-nav.opened ul.main-menu').toggle();
-    $('#main-nav.opened button.toggle').attr('aria-expanded', 'true');
-    $('#main-nav.opened').addClass('closed');
-    $('#main-nav.opened.closed').removeClass('opened');
-  });
-
-  $(document).keyup(function(e) {
-    if (e.keyCode == 27) {
-      // escape key maps to keycode `27`
-      $('#main-nav').hide();
+// Close on ESC
+$(function () {
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) { // escape key maps to keycode `27`
+      document.getElementById("toggle").setAttribute("aria-expanded", "false");
+      document.getElementById("main-menu").classList.remove("dblock");
     }
   });
 });
+
+// Toggle Aria
+function toggleAria() {
+
+  var ariastate = document.getElementById("toggle").getAttribute("aria-expanded");
+  if (ariastate == "true") {
+    document.getElementById("toggle").setAttribute("aria-expanded", "false");
+    document.getElementById("main-menu").classList.remove("dblock");
+  } else {
+    document.getElementById("toggle").setAttribute("aria-expanded", "true");
+    document.getElementById("main-menu").classList.add("dblock");
+  }
+
+}
