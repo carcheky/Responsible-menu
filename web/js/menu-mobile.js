6751636cbe95d2cc3796b502fcea8ee73c974fae
carcheky <carcheky@gmail.com>
2019-07-23 15:57:59 +0200

    pending task

--- web/js/menu-mobile.js
+++ web/js/menu-mobile.js
@@ -1,25 +1,24 @@
-$(function () {
-
+$(function() {
   $('#main-nav ul.main-menu').smartmenus();
 
-  $("#toggle.closed").click(function () {
-    $(".closed").addClass("opened");
-    $(".closed").attr("aria-expanded", "true");
-    $('.closed + ul.main-menu').toggle();
-    $(".closed").removeClass("closed");
+  $('.closed button.toggle').click(function() {
+    $('#main-nav.closed ul.main-menu').toggle();
+    $('#main-nav.closed button.toggle').attr('aria-expanded', 'true');
+    $('#main-nav.closed').addClass('opened');
+    $('#main-nav.opened.closed').removeClass('closed');
   });
 
-  $("#toggle.opened").click(function () {
-    $(".opened").addClass("closed");
-    $(".opened").attr("aria-expanded", "false");
-    $('.opened + ul.main-menu').toggle();
-    $(".opened").removeClass("opened");
+  $('.opened button.toggle').click(function() {
+    $('#main-nav.opened ul.main-menu').toggle();
+    $('#main-nav.opened button.toggle').attr('aria-expanded', 'true');
+    $('#main-nav.opened').addClass('closed');
+    $('#main-nav.opened.closed').removeClass('opened');
   });
 
-  $(document).keyup(function (e) {
-    if (e.keyCode == 27) { // escape key maps to keycode `27`
-      $("#main-nav ul.main-menu").hide()
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) {
+      // escape key maps to keycode `27`
+      $('#main-nav').hide();
     }
   });
-
 });
