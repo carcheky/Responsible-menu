18e8a6814cb18388e46ea92bef5dba5e6cca1c5c
carcheky <carcheky@gmail.com>
2019-08-01 09:40:40 +0200

    Update responsible-menu.js

--- web/js/responsible-menu.js
+++ web/js/responsible-menu.js
@@ -1,9 +1,15 @@
 // SmartMenus init
 $(function () {
   $('#main-menu').smartmenus({
+    mainMenuSubOffsetX: 1,
     mainMenuSubOffsetX: -1,
     subMenusSubOffsetX: 10,
-    subMenusSubOffsetY: 0
+    subMenusSubOffsetY: 0,
+    noMouseOver: true,
+    markCurrentItem: true,
+    markCurrentTree: true,
+    subMenusMinWidth: '100%'
+    // showOnClick: true
   });
 });
 
@@ -13,20 +19,23 @@ $(function () {
     if (e.keyCode == 27) { // escape key maps to keycode `27`
       document.getElementById("toggle").setAttribute("aria-expanded", "false");
       document.getElementById("main-menu").classList.remove("dblock");
+      document.getElementById("toggle").classList.remove("dblock");
     }
   });
 });
 
 // Toggle Aria
 function toggleAria() {
-
   var ariastate = document.getElementById("toggle").getAttribute("aria-expanded");
   if (ariastate == "true") {
-    document.getElementById("toggle").setAttribute("aria-expanded", "false");
+    // console.log('false, class dblock removed');
     document.getElementById("main-menu").classList.remove("dblock");
+    document.getElementById("toggle").classList.remove("dblock");
+    document.getElementById("toggle").setAttribute("aria-expanded", "false");
   } else {
-    document.getElementById("toggle").setAttribute("aria-expanded", "true");
+    // console.log('true, class dblock added');
     document.getElementById("main-menu").classList.add("dblock");
+    document.getElementById("toggle").classList.add("dblock");
+    document.getElementById("toggle").setAttribute("aria-expanded", "true");
   }
-
 }
